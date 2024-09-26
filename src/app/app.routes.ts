724da2853cdee2ab51be0ced6bd8a38078a10050
mamebassine <mamebassine06@gmail.com>
2024-09-26 17:33:23 +0000

    Terminer page categorie et contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 import { AccueilComponent } from './portail/accueil/accueil.component'; //MOI Mise à jour du chemin
 import { ProduitComponent } from './portail/produit/produit.component'; //MOI Mise à jour du chemin
+import { CategorieComponent } from './portail/categorie/categorie.component'; //MOI Mise à jour du chemin
+import { ContactComponent } from './portail/contact/contact.component'; //MOI Mise à jour du chemin
+
+
 
 
 
@@ -9,6 +13,10 @@ export const routes: Routes = [
     
     { path: '', component: AccueilComponent }, // MOI Route pour l'accueil
     { path: 'produit', component: ProduitComponent }, // MOI Route pour l'accueil
+    { path: 'categorie', component: CategorieComponent }, // MOI Route pour l'accueil
+    { path: 'contact', component: ContactComponent }, // MOI Route pour l'accueil
+
+
 
 
 ];
