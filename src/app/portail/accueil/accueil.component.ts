f54d2e7ca69c0a03da6a1023863149d88c69fa1a
mamebassine <mamebassine06@gmail.com>
2024-09-27 18:59:12 +0000

    Implentations du footer et header

--- src/app/portail/accueil/accueil.component.ts
+++ src/app/portail/accueil/accueil.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../header/header/header.component";
+import { FooterComponent } from "../../footer/footer/footer.component";
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
